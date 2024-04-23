4ee91412712c8b48c53c2afd7e0652e0f87e9fe0
Faizan Shabbir <faizanshabbir@gmail.com>
2024-04-23 07:57:53 -0500

    fetch data from sheets urls

--- app/payments/columns.tsx
+++ app/payments/columns.tsx
@@ -4,24 +4,70 @@ import { ColumnDef } from "@tanstack/react-table"
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
-export type Payment = {
-  id: string
-  amount: number
-  status: "pending" | "processing" | "success" | "failed"
-  email: string
+
+export type AwsInstance = {
+  instance_name: string
+  gpu: string
+  gpu_memory_gb: string
+  num_gpus: string
+  peer_to_peer: string
+  vcpus: string
+  instance_memory_gb: string
+  network_bandwidth: string
+  ebs_bandwidth_gbps: string
+  on_demand_price_hr: string
+  one_yr_reserved_price_hr: string
+  three_yr_reserved_price_hr: string
+  instance_storage: string
 }
 
-export const columns: ColumnDef<Payment>[] = [
+export const columns: ColumnDef<AwsInstance>[] = [
   {
-    accessorKey: "status",
-    header: "Status",
+    accessorKey: "instance_name",
+    header: "Instance Name",
   },
   {
-    accessorKey: "email",
-    header: "Email",
+    accessorKey: "gpu",
+    header: "GPU",
   },
   {
-    accessorKey: "amount",
-    header: "Amount",
+    accessorKey: "gpu_memory_gb",
+    header: "GPU Memory (GB)",
+  },
+  {
+    accessorKey: "num_gpus",
+    header: "Number of GPUs",
+  },
+  {
+    accessorKey: "vcpus",
+    header: "vCPUs",
+  },
+  {
+    accessorKey: "instance_memory_gb",
+    header: "Instance Memory (GB)",
+  },
+  {
+    accessorKey: "network_bandwidth",
+    header: "Network Bandwidth",
+  },
+  {
+    accessorKey: "ebs_bandwidth_gbps",
+    header: "EBS Bandwidth (Gbps)",
+  },
+  {
+    accessorKey: "on_demand_price_hr",
+    header: "On-Demand Price/Hr",
+  },
+  {
+    accessorKey: "one_yr_reserved_price_hr",
+    header: "1 Yr Reserved Price/Hr",
+  },
+  {
+    accessorKey: "three_yr_reserved_price_hr",
+    header: "3 Yr Reserved Price/Hr",
+  },
+  {
+    accessorKey: "instance_storage",
+    header: "Instance Storage",
   },
 ]
--- app/payments/page.tsx
+++ app/payments/page.tsx
@@ -1,22 +1,25 @@
-import { Payment, columns } from "./columns"
+import { AwsInstance, columns } from "./columns"
 import { DataTable } from "./data-table"
 
-async function getData(): Promise<Payment[]> {
+async function getAwsData(): Promise<AwsInstance[]> {
   // Fetch data from your API here.
-  return [
-    {
-      id: "728ed52f",
-      amount: 100,
-      status: "pending",
-      email: "m@example.com",
-    },
-    // ...
-  ]
+  const response = await fetch("https://sheetdb.io/api/v1/ddtqulvvu5lba", { next: { revalidate: 1800 } });
+  const data = await response.json();
+  return data;
+  // return [
+  //   {
+  //     id: "728ed52f",
+  //     amount: 100,
+  //     status: "pending",
+  //     email: "m@example.com",
+  //   },
+  //   // ...
+  // ]
 }
 
 export default async function DemoPage() {
-  const data = await getData()
-
+  const data = await getAwsData()
+  console.log(data)
   return (
     <div className="container mx-auto py-10">
       <DataTable columns={columns} data={data} />
