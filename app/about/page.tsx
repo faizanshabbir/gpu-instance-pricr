13f4a1de71476e557f2c018b438ec538ec568583
Faizan Shabbir <faizanshabbir@gmail.com>
2024-05-31 21:24:19 -0500

    change about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -10,7 +10,7 @@ export default function AboutPage() {
             </div>
             <div className="container mx-auto py-5 text-left">
                 <h1 className="font-bold">Why make this?</h1>
-                <p>At work, I was always struggling to find the right GPU instances for our projects. Ever spent ages trying to find centralized comparisons for instances in a cloud provider? Yeah, but even harder is isolating that information for all GPU instances in a cloud provider. That's why I put together this. </p>
+                <p>At work, I was always struggling to find the right GPU instances for our projects. That is why I put this together. Hope it helps you out at your megacorp job.</p>
                 <br/>
                 <p>However, as an individual developer, it can be more cost effective and quicker to use smaller cloud providers. The new entrants in AI / cloud instance space have compelling price points and are quick to setup. We will be adding that information soon. </p>
             </div>
